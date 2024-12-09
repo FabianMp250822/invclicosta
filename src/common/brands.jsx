beb352a43162c5d0a7d86182025c36ac8c1c13d5
FabianMp250822 <fabianmunzpuello@hotmail.com>
2024-12-09 13:29:28 -0500

    nuevos logos

--- src/common/brands.jsx
+++ src/common/brands.jsx
@@ -3,17 +3,26 @@ import React from "react";
 import styles from './Brands.module.css';  // Usamos el CSS module
 
 const brands_img = [
-  { img: "/assets/img/brand/11.webp", alt: "Brand 1" },
-  { img: "/assets/img/brand/2.webp", alt: "Brand 2" },
-  { img: "/assets/img/brand/3.webp", alt: "Brand 3" },
-  { img: "/assets/img/brand/4.webp", alt: "Brand 4" },
-  { img: "/assets/img/brand/5.webp", alt: "Brand 5" },
-  { img: "/assets/img/brand/6.webp", alt: "Brand 6" },
-  { img: "/assets/img/brand/7.webp", alt: "Brand 7" },
-  { img: "/assets/img/brand/8.webp", alt: "Brand 8" },
-  { img: "/assets/img/brand/9.webp", alt: "Brand 9" },
-  { img: "/assets/img/brand/10.webp", alt: "Brand 10" },
+  { img: "/assets/img/brand/AstraZeneca-100.jpg", alt: "Brand 1" },
+  { img: "/assets/img/brand/Bayer S.A.S-100.jpg", alt: "Brand 2" },
+  { img: "/assets/img/brand/BRISTOL-100.jpg", alt: "Brand 3" },
+  { img: "/assets/img/brand/Fortrea-100.jpg", alt: "Brand 4" },
+  { img: "/assets/img/brand/GlaxoSmithKline -100.jpg", alt: "Brand 5" },
+  { img: "/assets/img/brand/INTRIAL S.A.S-100.jpg", alt: "Brand 6" },
+  { img: "/assets/img/brand/MSD-100.jpg", alt: "Brand 7" },
+  { img: "/assets/img/brand/Novartis-100.jpg", alt: "Brand 8" },
+  { img: "/assets/img/brand/Novo Nordisk-100.jpg", alt: "Brand 9" },
+  { img: "/assets/img/brand/Parexel-100.jpg", alt: "Brand 10" },
+  { img: "/assets/img/brand/PSI CRO -100.jpg", alt: "Brand 11" },
+  { img: "/assets/img/brand/Resolution Latin-100.jpg", alt: "Brand 12" },
+  { img: "/assets/img/brand/ROCHE-100.jpg", alt: "Brand 13" },
+  { img: "/assets/img/brand/Sanofi -100.jpg", alt: "Brand 14" },
+  { img: "/assets/img/brand/Syneos-100.jpg", alt: "Brand 15" },
+  { img: "/assets/img/brand/Syneos_1-100.jpg", alt: "Brand 16" },
+  { img: "/assets/img/brand/The George Washington-100.jpg", alt: "Brand 17" },
 ];
+
+
 
 const Brands = ({ style_2 = false, border_style = false }) => {
   return (
